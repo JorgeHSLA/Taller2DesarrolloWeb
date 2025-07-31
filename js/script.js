e5630eb6721bfd4a58aa2cd2f4ff659caa6c6586
JORGE H SiERRA L <jorgesierralaiton@gmail.com>
2025-07-31 14:04:13 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#form")
+const nombreForm = document.querySelector("#txtNombre");
+const atributo1 = document.querySelector("#txtAtributo1");
+const atributo2 = document.querySelector("#txtAtributo2");
+const atributo3 = document.querySelector("#txtAtributo3");
+const imagen = document.querySelector("#txtImage");
+const precio = document.querySelector("#txtPrecio");
+
+console.log({
+    form
+});
